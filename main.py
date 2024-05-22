00c70805ff1a003ab805e69a920cdb0452032563
CLL <96326019+CllsPy@users.noreply.github.com>
2024-05-22 10:35:22 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+import time
+import streamlit as st
+
+st.title("Hoop Web App")
+
+col1, col2 = st.columns(2)
+
+# Tipo de análise
+tipo_analise = col1.radio(
+	'selecione o tipo de análise', 
+	["mecânica","condutividade"]
+)
+
+
+# Modelo
+tipo_analise = col2.multiselect(
+	'modelos', 
+	["voigt","reuss"]
+)
